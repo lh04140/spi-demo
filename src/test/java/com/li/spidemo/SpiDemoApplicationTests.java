26308fd5a1211e08b68955c09ac3e6f90cdbf6d4
70642 <706422117@qq.com>
2022-10-28 01:56:01 +0800

    first  commmit

---
+++ src/test/java/com/li/spidemo/SpiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.li.spidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
